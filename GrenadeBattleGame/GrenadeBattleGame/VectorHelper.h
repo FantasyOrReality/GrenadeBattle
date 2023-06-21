0be688bf58a884caf8eaf86050935612e9099003
FantasyOrReality <99820988+FantasyOrReality@users.noreply.github.com>
2023-06-21 17:00:12 +0100

    moved comment

--- GrenadeBattleGame/GrenadeBattleGame/VectorHelper.h
+++ GrenadeBattleGame/GrenadeBattleGame/VectorHelper.h
@@ -7,6 +7,7 @@ namespace VectorHelper
 	float SquareMagnitude(sf::Vector2f vec);
 	float Magnitude(sf::Vector2f vec);
 
+	//Practical Task - Reflection
 	sf::Vector2f Normalise(sf::Vector2f vec);
 
 	float Dot(sf::Vector3f a, sf::Vector3f b);
@@ -15,7 +16,6 @@ namespace VectorHelper
 	sf::Vector3f Cross(sf::Vector3f a, sf::Vector3f b);
 	//sf::Vector2f Cross(sf::Vector2f a, sf::Vector2f b);
 	
-	//Practical Task - Reflection
 	sf::Vector3f GetReflection(sf::Vector3f incident, sf::Vector3f normal);
 	sf::Vector2f GetReflection(sf::Vector2f incident, sf::Vector2f normal);
 
